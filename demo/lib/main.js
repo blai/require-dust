c4c210173b3ffbb7a61c621dbfcabb340ce69afb
Brian Lai <brian.lai.ym@gmail.com>
2012-06-21 14:14:06 -0700

    add example

--- demo/lib/main.js
+++ demo/lib/main.js
@@ -1,19 +1,20 @@
 require.config({
-    paths: {
-        rdust: '../../require-dust',
-        dust: 'dust-full-0.6.0'
-    }
+	paths: {
+		rdust: '../../require-dust',
+		dust: 'dust-full-0.6.0'
+	}
 });
 
 define(['dust', 'rdust!template/test'], function(dust, template) {
-    var data = {
-        "root": "Subject",
-        "person": {
-            "name": "Larry",
-            "age": 45
-        }
-    };
-    dust.render('template/test', data, function(err, out) {
-        console.log(out);
-    })
+	var data = {
+		"root": "Subject",
+		"person": {
+			"name": "Larry",
+			"age": 45
+		}
+	};
+	dust.render('template/test', data, function(err, out) {
+		console.info(out);
+		document.getElementById('content').innerHTML = out;
+	})
 });
